dfa927ccd625131a4bb3f7e04522a9ad5950bdc1
Tim <zygurt@gmail.com>
2025-10-24 22:59:31 +1000

    USB MIDI minor PCB tweaks

--- src/Pico/PicoSax/PicoSax.c
+++ src/Pico/PicoSax/PicoSax.c
@@ -5,35 +5,32 @@
 #include "hardware/uart.h"
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
-//#include "pico/binary_info.h"
+#include "pico/binary_info.h" //USBMIDI
 
 
 #include "PicoSaxDefines.h"
 #include "PicoSaxFunc.h"
 
-//#include "bsp/board.h"
-//#include "tusb.h"
+#include "bsp/board.h" //USBMIDI
+#include "tusb.h" //USBMIDI
 
-// void midi_task(void);
+enum  {
+  BLINK_NOT_MOUNTED = 250,
+  BLINK_MOUNTED = 1000,
+  BLINK_SUSPENDED = 2500,
+};
 
-// // Invoked when usb bus is suspended
-// // remote_wakeup_en : if host allow us  to perform remote wakeup
-// // Within 7ms, device must draw an average of current less than 2.5 mA from bus
-// void tud_suspend_cb(bool remote_wakeup_en)
-// {
-//   (void) remote_wakeup_en;
-//   blink_interval_ms = BLINK_SUSPENDED;
-// }
+static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
+const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 
-// // Invoked when usb bus is resumed
-// void tud_resume_cb(void)
-// {
-//   blink_interval_ms = BLINK_MOUNTED;
-// }
+void midi_task(void);
+
+
 
 int main()
 {
-    stdio_init_all();
+    //stdio_init_all();
+    
     // tusb_init();
     // PicoSax Setup
     setupBoard();
@@ -43,13 +40,19 @@ int main()
     adc_select_input(0);
     setupBreath();
     // Set up our UART
-    gpio_set_function(UART_TX_PIN, UART_FUNCSEL_NUM(uart0, 0));
-    gpio_set_function(UART_RX_PIN, UART_FUNCSEL_NUM(uart0, 1));
-    uart_init(UART_ID, BAUD_RATE);
+    // gpio_set_function(UART_TX_PIN, UART_FUNCSEL_NUM(uart0, 0));
+    // gpio_set_function(UART_RX_PIN, UART_FUNCSEL_NUM(uart0, 1));
+    // uart_init(UART_ID, BAUD_RATE);
+
+    board_init(); //USBMIDI
+    tusb_init(); //USBMIDI
     
 
     while (true)
     {
+        tud_task(); // tinyusb device task
+        uint8_t msg[3];
+
         uint8_t note_midi;
         note_midi = readButtons();
         uint8_t breath_filt;
@@ -68,12 +71,12 @@ int main()
         if (breath_filt < breath_threshold && note_on == 1){
             note_on = 0;
             gpio_put(PICO_DEFAULT_LED_PIN, 0);
-            MIDInoteOff(1, prev_note, breath_raw_midi);
+            // MIDInoteOff(1, prev_note, breath_raw_midi);
 
-            // msg[0] = 0x80;                    // Note Off - Channel 1
-            // msg[1] = prev_note; // Note Number
-            // msg[2] = 0;                       // Velocity
-            // tud_midi_n_stream_write(0, 0, msg, 3);
+            msg[0] = 0x80;                    // Note Off - Channel 1
+            msg[1] = prev_note; // Note Number
+            msg[2] = 0;                       // Velocity
+            tud_midi_n_stream_write(0, 0, msg, 3);
 
             retrigger_flag = 0;
         }
@@ -87,28 +90,60 @@ int main()
             if(note_midi != prev_note){
                 //Breath and new note || Breath and same note
                 // turn off previous note
-                // msg[0] = 0x80;                    // Note Off - Channel 1
-                // msg[1] = prev_note; // Note Number
-                // msg[2] = 0;                       // Velocity
-                // tud_midi_n_stream_write(0, 0, msg, 3);
-                MIDInoteOff(1, prev_note, breath_raw_midi);
+                msg[0] = 0x80;                    // Note Off - Channel 1
+                msg[1] = prev_note; // Note Number
+                msg[2] = breath_raw_midi;                       // Velocity
+                tud_midi_n_stream_write(0, 0, msg, 3);
+                // MIDInoteOff(1, prev_note, breath_raw_midi);
                 note_on = 0;
             }
             // turn on new note
-            // msg[0] = 0x90;                    // Note On - Channel 1
-            // msg[1] = note_midi;                // Note Number
-            // msg[2] = breath_raw_midi;           // Velocity
-            // tud_midi_n_stream_write(0, 0, msg, 3);
+            msg[0] = 0x90;                    // Note On - Channel 1
+            msg[1] = note_midi;                // Note Number
+            msg[2] = breath_raw_midi;           // Velocity
+            tud_midi_n_stream_write(0, 0, msg, 3);
 
-            MIDInoteOn(1, note_midi, breath_raw_midi);
+            // MIDInoteOn(1, note_midi, breath_raw_midi);
             note_on = 1;
             //uart_puts(UART_ID, &note_midi);
             if(VERBOSE){
-                printf("%d\n",note_midi);
+                // printf("%d\n",note_midi);
             }
             prev_note = note_midi;
         }
-        prev_breath = breath_filt;
-        sleep_ms(10); // Delay to reduce amount of midi data
+        prev_breath = breath_filt; 
+        //sleep_ms(50); // Delay to reduce amount of midi data
     }
+}
+
+
+//--------------------------------------------------------------------+
+// Device callbacks
+//--------------------------------------------------------------------+
+
+// Invoked when device is mounted
+void tud_mount_cb(void)
+{
+  blink_interval_ms = BLINK_MOUNTED;
+}
+
+// Invoked when device is unmounted
+void tud_umount_cb(void)
+{
+  blink_interval_ms = BLINK_NOT_MOUNTED;
+}
+
+// Invoked when usb bus is suspended
+// remote_wakeup_en : if host allow us  to perform remote wakeup
+// Within 7ms, device must draw an average of current less than 2.5 mA from bus
+void tud_suspend_cb(bool remote_wakeup_en)
+{
+  (void) remote_wakeup_en;
+  blink_interval_ms = BLINK_SUSPENDED;
+}
+
+// Invoked when usb bus is resumed
+void tud_resume_cb(void)
+{
+  blink_interval_ms = BLINK_MOUNTED;
 }
---
+++ src/Pico/PicoUSBMIDI/PicoUSBMIDI.c
@@ -0,0 +1,194 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/uart.h"
+
+
+#include "blink.pio.h"
+
+//USBMIDI
+#include "pico/binary_info.h" 
+
+#include "bsp/board.h" 
+#include "tusb.h" 
+enum  {
+  BLINK_NOT_MOUNTED = 250,
+  BLINK_MOUNTED = 1000,
+  BLINK_SUSPENDED = 2500,
+};
+
+static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
+const uint LED_PIN = 2;
+
+void led_blinking_task(void);
+void midi_task(void);
+
+//END USBMIDI
+
+void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
+    blink_program_init(pio, sm, offset, pin);
+    pio_sm_set_enabled(pio, sm, true);
+
+    printf("Blinking pin %d at %d Hz\n", pin, freq);
+
+    // PIO counter program takes 3 more cycles in total than we pass as
+    // input (wait for n + 1; mov; jmp)
+    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
+}
+
+// UART defines
+// By default the stdout UART is `uart0`, so we will use the second one
+#define UART_ID uart1
+#define BAUD_RATE 115200
+
+// Use pins 4 and 5 for UART1
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define UART_TX_PIN 4
+#define UART_RX_PIN 5
+
+
+
+int main()
+{
+    // stdio_init_all();
+
+    // // PIO Blinking example
+    // PIO pio = pio0;
+    // uint offset = pio_add_program(pio, &blink_program);
+    // printf("Loaded program at %d\n", offset);
+    
+    // #ifdef PICO_DEFAULT_LED_PIN
+    // blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
+    // #else
+    // blink_pin_forever(pio, 0, offset, 6, 3);
+    // #endif
+    // // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
+
+    // // Set up our UART
+    // uart_init(UART_ID, BAUD_RATE);
+    // // Set the TX and RX pins by using the function select on the GPIO
+    // // Set datasheet for more information on function select
+    // gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    // gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+    
+    // // Use some the various UART functions to send out data
+    // // In a default system, printf will also output via the default UART
+    
+    // // Send out a string, with CR/LF conversions
+    // uart_puts(UART_ID, " Hello, UART!\n");
+    
+    // // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
+
+
+    board_init();
+
+    tusb_init();
+
+
+    while (true) {
+        printf("Hello, world!\n");
+
+        tud_task(); // tinyusb device task
+        led_blinking_task();
+        midi_task();
+
+        //sleep_ms(1000);
+    }
+}
+
+//--------------------------------------------------------------------+
+// Device callbacks
+//--------------------------------------------------------------------+
+
+// Invoked when device is mounted
+void tud_mount_cb(void)
+{
+  blink_interval_ms = BLINK_MOUNTED;
+}
+
+// Invoked when device is unmounted
+void tud_umount_cb(void)
+{
+  blink_interval_ms = BLINK_NOT_MOUNTED;
+}
+
+// Invoked when usb bus is suspended
+// remote_wakeup_en : if host allow us  to perform remote wakeup
+// Within 7ms, device must draw an average of current less than 2.5 mA from bus
+void tud_suspend_cb(bool remote_wakeup_en)
+{
+  (void) remote_wakeup_en;
+  blink_interval_ms = BLINK_SUSPENDED;
+}
+
+// Invoked when usb bus is resumed
+void tud_resume_cb(void)
+{
+  blink_interval_ms = BLINK_MOUNTED;
+}
+
+//--------------------------------------------------------------------+
+// MIDI Task
+//--------------------------------------------------------------------+
+
+// Variable that holds the current position in the sequence.
+uint32_t note_pos = 0;
+
+// Store example melody as an array of note values
+uint8_t note_sequence[] =
+{
+  74,78,81,86,90,93,98,102,57,61,66,69,73,78,81,85,88,92,97,100,97,92,88,85,81,78,
+  74,69,66,62,57,62,66,69,74,78,81,86,90,93,97,102,97,93,90,85,81,78,73,68,64,61,
+  56,61,64,68,74,78,81,86,90,93,98,102
+};
+
+void midi_task(void)
+{
+  static uint32_t start_ms = 0;
+  uint8_t msg[3];
+
+  // send note every 1000 ms
+  if (board_millis() - start_ms < 286) return; // not enough time
+  start_ms += 286;
+
+  // Previous positions in the note sequence.
+  int previous = note_pos - 1;
+
+  // If we currently are at position 0, set the
+  // previous position to the last note in the sequence.
+  if (previous < 0) previous = sizeof(note_sequence) - 1;
+
+  // Send Note On for current position at full velocity (127) on channel 1.
+  msg[0] = 0x90;                    // Note On - Channel 1
+  msg[1] = note_sequence[note_pos]; // Note Number
+  msg[2] = 127;                     // Velocity
+  tud_midi_n_stream_write(0, 0, msg, 3);
+
+  // Send Note Off for previous note.
+  msg[0] = 0x80;                    // Note Off - Channel 1
+  msg[1] = note_sequence[previous]; // Note Number
+  msg[2] = 0;                       // Velocity
+  tud_midi_n_stream_write(0, 0, msg, 3);
+
+  // Increment position
+  note_pos++;
+
+  // If we are at the end of the sequence, start over.
+  if (note_pos >= sizeof(note_sequence)) note_pos = 0;
+}
+
+//--------------------------------------------------------------------+
+// BLINKING TASK
+//--------------------------------------------------------------------+
+void led_blinking_task(void)
+{
+  static uint32_t start_ms = 0;
+  static bool led_state = false;
+
+  // Blink every interval ms
+  if ( board_millis() - start_ms < blink_interval_ms) return; // not enough time
+  start_ms += blink_interval_ms;
+
+  board_led_write(led_state);
+  led_state = 1 - led_state; // toggle
+}
